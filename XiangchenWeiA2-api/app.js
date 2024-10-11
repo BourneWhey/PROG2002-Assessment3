86ee3e6af2a0e75d5f79f31516b05246fd3be379
BourneWhey <145343860+BourneWhey@users.noreply.github.com>
2024-10-12 00:25:56 +0800

    create donation and fundraiser

--- XiangchenWeiA2-api/app.js
+++ XiangchenWeiA2-api/app.js
@@ -73,6 +73,94 @@ app.get("/fundraiser/donations/:fundraiserId", (req, res) => {
   })
 });
 
+app.post("/fundraiser/donations", (req, res) => {
+  if (Number.isNaN(req.body.amount)) {
+    res.status(400).send("Amount should be number.")
+    return
+  }
+
+  if (req.body.amount < 5) {
+    res.status(400).send("The amount should be greater than 5.")
+    return
+  }
+
+  connection.query('insert info donation(DATE, AMOUNT, GIVER, FUNDRAISER_ID) values(?,?,?,?)',
+    [new Date(), req.body.giver, req.body.amount, req.body.fundraiserId], (err, records, fields) => {
+    if (err) {
+      console.log("Error while retrieve the data");
+    } else {
+      res.send(records)
+    }
+  })
+});
+
+app.post("/fundraiser", (req, res) => {
+  if (!req.body.organizer) {
+    res.status(400).send("Organizer required.")
+    return
+  }
+
+  if (!req.body.caption) {
+    res.status(400).send("Caption required.")
+    return
+  }
+
+  if (!req.body.targetFunding) {
+    res.status(400).send("Target funding required.")
+    return
+  }
+
+  if (!req.body.currentFunding) {
+    res.status(400).send("Current funding required.")
+    return
+  }
+
+  if (!req.body.city) {
+    res.status(400).send("City required.")
+    return
+  }
+
+  if (!req.body.categoryId) {
+    res.status(400).send("CategoryId required.")
+    return
+  }
+
+  if (Number.isNaN(req.body.targetFunding)) {
+    res.status(400).send("Target funding should be number.")
+    return
+  }
+
+  if (Number.isNaN(req.body.currentFunding)) {
+    res.status(400).send("Current funding should be number.")
+    return
+  }
+
+  connection.query('insert info fundraiser(ORGANIZER, CAPTION, TARGET_FUNDING, CURRENT_FUNDING, CITY, ACTIVE, CATEGORY_ID) values(?,?,?,?,?,?,?)',
+    [req.body.organizer, req.body.caption, req.body.targetFunding, req.body.currentFunding, req.body.city, req.body.active, req.body.categoryId], (err, records, fields) => {
+      if (err) {
+        console.log("Error while retrieve the data");
+      } else {
+        res.send(records)
+      }
+    })
+});
+
+
+app.post("/fundraiser/donations", (req, res) => {
+  var date = new Date()
+  var giver = req.body.giver
+  var amount = req.body.amount
+  var fundraiserId = req.body.fundraiserId
+
+  connection.query('INSERT INTO donation(DATE, AMOUNT, GIVER, FUNDRAISER_ID) VALUES(?,?,?,?)', [date, amount, giver, fundraiserId], (err, records) => {
+    if (err) {
+      console.log("Query error", err);
+    } else {
+      res.send(records)
+    }
+  });
+});
+
 app.listen(port, function() {
   console.log(`App listening on port ${port}`);
 })
